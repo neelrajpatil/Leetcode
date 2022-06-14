f5d226540c38aa9b5368081d41ef6f743a512ed8
neelrajpatil <Neelrajpatil@ymail.com>
2022-06-14 20:09:29 +0530

    Time: 206 ms (44.80%), Space: 16.8 MB (49.73%) - LeetHub

---
+++ 136-single-number/136-single-number.py
@@ -0,0 +1,24 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+
+        # O(1) space complexity O(nlogn + n) time complexity
+        nums.sort()
+        i=0
+        length = len(nums)
+        while(i<length):
+            if (i+1 == length):
+                return nums[i]
+            if nums[i]==nums[i+1]:
+                i = i+2
+            else:
+                return nums[i]
+        
+        
+        # O(n) space complexity O(n) time complexity
+        # ans = []
+        # for num in nums:
+        #     if num not in ans:
+        #         ans.append(num)
+        #     else:
+        #         ans.remove(num)
+        # return ans[0]        
