d2936ba70507aa1801b341fa2da1edbf9d1a010c
neelrajpatil <Neelrajpatil@ymail.com>
2022-07-26 20:20:19 +0530

    Time: 421 ms (79.22%), Space: 22.9 MB (17.96%) - LeetHub

--- 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.py
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.py
@@ -1,12 +1,27 @@
 class Solution:
     def findDuplicates(self, nums: List[int]) -> List[int]:
+        
+        # Hashmap of each element O(n) time complexity and O(n) space complexity
         ans = []
+        count = {}
         
         for element in nums:
-            if nums[abs(element)-1] < 0:
-                ans.append(abs(element))
-            
-            nums[abs(element)-1] *= -1 
+            if element in count:
+                count[element] += 1
+                ans.append(element)
+            else:
+                count[element] = 1
+
         return ans
+#         O(n) time complexity single pass and O(1) space complexity 
+#         ans = []       
+#         for element in nums:
+#             if nums[abs(element)-1] < 0:
+#                 ans.append(abs(element))
+#             nums[abs(element)-1] *= -1 
+#         return ans
+        
+    
+        
         
         
