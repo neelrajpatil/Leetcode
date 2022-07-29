af0568b7a84b97040c4f2de8e7ab7ae2f0a1f211
neelrajpatil <Neelrajpatil@ymail.com>
2022-07-29 17:33:10 +0530

    Time: 61 ms (34.66%), Space: 13.8 MB (74.54%) - LeetHub

--- 48-rotate-image/48-rotate-image.py
+++ 48-rotate-image/48-rotate-image.py
@@ -3,26 +3,26 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
+        matrix.reverse()
+        for i in range(len(matrix)):
+            for j in range(i+1, len(matrix)):
+                matrix[i][j] , matrix[j][i] = matrix[j][i], matrix[i][j]
+
+
+#         left, right = 0 , len(matrix) - 1
         
-        left, right = 0 , len(matrix) - 1
-        
-        while (left<right):
+#         while (left<right):
             
-            for i in range(right-left):
-                top, bottom = left, right
-                temp = matrix[top][left+i]
+#             for i in range(right-left):
+#                 top, bottom = left, right
+#                 temp = matrix[top][left+i]
                 
-                matrix[top][left+i] = matrix[bottom-i][left]
-                matrix[bottom-i][left] = matrix[bottom][right-i]
-                matrix[bottom][right-i] = matrix[top+i][right]
-                matrix[top+i][right] = temp
+#                 matrix[top][left+i] = matrix[bottom-i][left]
+#                 matrix[bottom-i][left] = matrix[bottom][right-i]
+#                 matrix[bottom][right-i] = matrix[top+i][right]
+#                 matrix[top+i][right] = temp
             
-            left += 1
-            right -= 1
+#             left += 1
+#             right -= 1
         
-#         matrix.reverse()
-#         for i in range(len(matrix)):
-#             for j in range(i+1, len(matrix)):
-#                 matrix[i][j] , matrix[j][i] = matrix[j][i], matrix[i][j]
-                
-        
+       
