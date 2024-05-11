d9e93ba3a465790285710ac4178784945d2cf85c
neelrajpatil <Neelrajpatil@ymail.com>
2024-05-10 22:45:32 -0400

    Time: 40 ms (91.73%) | Memory: 17 MB (64.69%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -3,26 +3,31 @@ class Solution:
         if len(s) != len(t):
             return False
 
-        ## Approach 1: 
-        s_dict = dict() 
-        # single pass s
-        for i in range(len(s)):
-            #make dict with char as  and count as value
-            s_dict[s[i]] = 1 + s_dict.get(s[i],0)
-        print(s_dict)
-
-
-        # single pass t
-        for i in range(len(t)):
-            # remove every char from dict, if not in dict return false
-            if s_dict.get(t[i],0) == 0:
-                return False
-            s_dict[t[i]] = s_dict.get(t[i],0) - 1
-
-        
-        # if dict is empty
-        if set(s_dict.values())== set([0]):
+        ## Approach 2:
+        if Counter(s) == Counter(t):
             return True
         return False
+
+        # ## Approach 1: Count chars in one string, decrement from other string.
+        # s_dict = dict() 
+        # # single pass s
+        # for i in range(len(s)):
+        #     #make dict with char as  and count as value
+        #     s_dict[s[i]] = 1 + s_dict.get(s[i],0)
+        # print(s_dict)
+
+
+        # # single pass t
+        # for i in range(len(t)):
+        #     # remove every char from dict, if not in dict return false
+        #     if s_dict.get(t[i],0) == 0:
+        #         return False
+        #     s_dict[t[i]] = s_dict.get(t[i],0) - 1
+
+        
+        # # if dict is empty
+        # if set(s_dict.values())== set([0]):
+        #     return True
+        # return False
 
         
