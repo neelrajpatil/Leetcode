4d2c334f9b93678e6fde8b81c84e4cabf856ac3e
neelrajpatil <Neelrajpatil@ymail.com>
2024-05-19 20:57:08 -0400

    Time: 94 ms (39.79%) | Memory: 22.3 MB (21.54%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,11 +1,23 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        all_sets = {}
-        for word in strs:
-            curr_set = tuple(sorted(word))
-            if curr_set in all_sets.keys():
-                all_sets[curr_set].append(word)
-            else:
-                all_sets[curr_set] = [word]
         
-        return all_sets.values()
+        # Approach 2 O(m*n)
+        res = defaultdict(list)
+        for word in strs:
+            chars = [0]*26
+            for char in word:
+                chars[ord(char)-ord('a')] += 1
+            res[tuple(chars)].append(word)
+
+        return res.values()
+
+        
+        # # Approach 1 O(m*n*log n)
+        # all_sets = {}
+        # for word in strs:
+        #     curr_set = tuple(sorted(word))
+        #     if curr_set in all_sets.keys():
+        #         all_sets[curr_set].append(word)
+        #     else:
+        #         all_sets[curr_set] = [word]
+        # return all_sets.values()
