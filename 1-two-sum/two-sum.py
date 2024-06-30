4564ac85a171bcd01de3cbb43289f5ac51a1fc26
Neelraj Patil <patilneelraj@gmail.com>
2024-06-30 10:18:07 -0400

    Time: 52 ms (89.82%) | Memory: 17.9 MB (7.66%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,13 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # store the complement as the key and the index of the og as the value in the key
-
-        prev = {}
-
-        for i in range(len(nums)):
-            if (target - nums[i]) in prev:
-                return [i,prev[(target - nums[i])]]
-            prev[nums[i]] = i
-
-        return []
-
+        compliment = {}
+        for idx, num in enumerate(nums):
+            # check 
+            if num in compliment.keys():
+                return (idx, compliment[num])
+            # Add compliment
+            compliment[target-num] = idx
+        return ()
