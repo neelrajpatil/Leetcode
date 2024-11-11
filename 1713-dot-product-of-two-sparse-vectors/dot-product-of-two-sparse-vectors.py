ed7ef0bdb83267ca4d8fba0b3ca9f8a05f82ec73
Neelraj Patil <patilneelraj@gmail.com>
2024-11-11 16:46:34 -0500

    Time: 1282 ms (59.60%) | Memory: 21.4 MB (12.03%) - LeetSync

--- 1713-dot-product-of-two-sparse-vectors/dot-product-of-two-sparse-vectors.py
+++ 1713-dot-product-of-two-sparse-vectors/dot-product-of-two-sparse-vectors.py
@@ -1,10 +1,9 @@
 class SparseVector:
     def __init__(self, nums: List[int]):
-        self.values = {}
-        self.len = len(nums)
+        self.pairs = [] #[[index,value]]
         for i in range(len(nums)):
             if nums[i] != 0:
-                self.values[i] = nums[i]
+                self.pairs.append([i,nums[i]])
 
     # Return the dotProduct of two sparse vectors
     def dotProduct(self, vec: 'SparseVector') -> int:
@@ -27,10 +26,25 @@ class SparseVector:
 
         """
         result = 0
-        # iterate through the items in one dict
-        for index, val in self.values.items():
-            if index in vec.values.keys(): # if the same index is in other dict
-                result += self.values[index] * vec.values[index] # dot product
+        self_p = 0
+        vec_p = 0
+        
+        # iterate through the pointers
+        while self_p < len(self.pairs) and vec_p < len(vec.pairs):
+            self_i , self_val = self.pairs[self_p]
+            vec_i, vec_val = vec.pairs[vec_p]
+            # if both indices are the same
+            if self_i == vec_i:
+                result += self_val * vec_val
+                vec_p += 1
+                self_p += 1
+            
+            # if self is ahead
+            elif self_i > vec_i:
+                vec_p += 1
+            # if vec is ahead
+            elif vec_i > self_i:
+                self_p += 1
                 
         
         return result
@@ -67,7 +81,42 @@ class SparseVector:
     #     for num1, num2 in zip(self.array, vec.array):
     #         result += num1*num2
     #     return result
-    # 1b. Hash Table (Store in a dict {'index':num,...}) (Dot product, iterate through one dict check if val is in other dict)
+    # # 1b. Hash Table (Store in a dict {'index':num,...}) (Dot product, iterate through one dict check if val is in other dict)
+    #     def __init__(self, nums: List[int]):
+    #     self.values = {}
+    #     self.len = len(nums)
+    #     for i in range(len(nums)):
+    #         if nums[i] != 0:
+    #             self.values[i] = nums[i]
+
+    # # Return the dotProduct of two sparse vectors
+    # def dotProduct(self, vec: 'SparseVector') -> int:
+    #     """
+    #     Test Cases
+    #         - [1,0,0,2,3], [0,3,0,4,8]
+    #             8
+    #         - [0,1,0,0,0] [0,0,0,0,2]
+    #             0
+    #         - [0,1,0,0,2,0,0] [1,0,0,0,3,0,4]
+
+    #     Constraints
+    #         - n == self.nums.length == vic.nums.length
+    #         - 1 <= n <= 10^5
+    #         - 0 self.nums[i], vic.nums[i]<= 100
+    #         - Store EFFICIENTLY
+
+    #     Assumptions
+    #         - Storage efficiency is coming at a cost of computational efficiency
+
+    #     """
+    #     result = 0
+    #     # iterate through the items in one dict
+    #     for index, val in self.values.items():
+    #         if index in vec.values.keys(): # if the same index is in other dict
+    #             result += self.values[index] * vec.values[index] # dot product
+                
+        
+    #     return result
     # 1c. Two Pointer (Store index, value pairs) two pointers in each array 
 
 # Approach 2: Compress and store, decompress right before dot product
