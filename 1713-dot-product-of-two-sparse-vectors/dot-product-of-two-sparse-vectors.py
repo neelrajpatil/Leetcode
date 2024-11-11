5a471e0ef6d3b0cf5775c10856fb781cea111cab
Neelraj Patil <patilneelraj@gmail.com>
2024-11-11 16:25:15 -0500

    Time: 1272 ms (75.32%) | Memory: 20.9 MB (35.74%) - LeetSync

---
+++ 1713-dot-product-of-two-sparse-vectors/dot-product-of-two-sparse-vectors.py
@@ -0,0 +1,43 @@
+class SparseVector:
+    def __init__(self, nums: List[int]):
+        self.array = nums 
+
+    # Return the dotProduct of two sparse vectors
+    def dotProduct(self, vec: 'SparseVector') -> int:
+        """
+        Test Cases
+            - [1,0,0,2,3], [0,3,0,4,8]
+                8
+            - [0,1,0,0,0] [0,0,0,0,2]
+                0
+            - [0,1,0,0,2,0,0] [1,0,0,0,3,0,4]
+
+        Constraints
+            - n == self.nums.length == vic.nums.length
+            - 1 <= n <= 10^5
+            - 0 self.nums[i], vic.nums[i]<= 100
+            - Store EFFICIENTLY
+
+        Assumptions
+            - Storage efficiency is coming at a cost of computational efficiency
+
+        """
+        result = 0
+        for num1, num2 in zip(self.array, vec.array):
+            result += num1*num2
+        return result
+
+
+# Approach 1: Try to compress and store, and compress in such a way that dot product can be performed without decompressing
+    # 1a. Brute Force (store in array, usual dot produxt)
+    # 1b. Hash Table (Store in a dict {'index':num,...}) (Dot product, iterate through one dict check if val is in other dict)
+    # 1c. Two Pointer (Store index, value pairs) two pointers in each array 
+
+# Approach 2: Compress and store, decompress right before dot product
+    # Compression: Store nums list [1,2,0,0,0,2] as [1,2,tuple(3),2]
+        
+
+# Your SparseVector object will be instantiated and called as such:
+# v1 = SparseVector(nums1)
+# v2 = SparseVector(nums2)
+# ans = v1.dotProduct(v2)
