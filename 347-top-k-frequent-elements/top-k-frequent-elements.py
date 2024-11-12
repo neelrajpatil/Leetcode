2f9cd628ea1287e6b51750b6a0aad13ecd7de0a1
Neelraj Patil <patilneelraj@gmail.com>
2024-11-12 16:07:36 -0500

    Time: 11 ms (25.45%) | Memory: 23.3 MB (5.53%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -21,23 +21,54 @@ class Solution:
         Assumptions
 
         """
-        # Approach 1: freq counter in one pass, sort freq_counter, get top k
-        # Time Complexity O(n Log n)
-        # Space Complexity O(n Log n)
+        # # Approach 1: freq counter in one pass, sort freq_counter, get top k
+        # # Time Complexity O(n Log n)
+        # # Space Complexity O(n Log n)
 
-        freq_c = defaultdict(int) # {value:count}
-        # create freq counter
+        # freq_c = defaultdict(int) # {value:count}
+        # # create freq counter
+        # for num in nums:
+        #     freq_c[num] += 1
+
+        # # turn dict into list
+        # freq_q_list = list(freq_c.items())
+        # # print(freq_q_list)
+
+        # # sort list on first second element
+        # final_list = sorted(freq_q_list, key=lambda x:x[1], reverse = True)
+
+        # return [f[0] for f in final_list[0:k] ]
+
+        # Approach 2: freq counter in one pass, create a ordered array from the freq_counter in one pass
+        # Time Complexity O(n+n) = O(n)
+        # Space Complexity O(n)
+
+        # freq counter
+        freq_c = defaultdict(int)
         for num in nums:
             freq_c[num] += 1
 
-        # turn dict into list
-        freq_q_list = list(freq_c.items())
-        # print(freq_q_list)
+        # ordered list
+        ord_list = [[] for i in range(len(nums)+1)] # example: 2nd position has a list of numbers repeated twice
 
-        # sort list on first second element
-        final_list = sorted(freq_q_list, key=lambda x:x[1], reverse = True)
+        for num,freq in freq_c.items():
+            ord_list[freq].append(num)
 
-        return [f[0] for f in final_list[0:k] ]
+        # get top k answers
+        result = []
+        for i in range(len(nums),0,-1):
+            while len(ord_list[i]) > 0:
+                result.append(ord_list[i].pop(0))
+                k -= 1
+                if k == 0:
+                    return result
+
+        return result
+
+        
+
+
+
         
         
 
