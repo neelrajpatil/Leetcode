6a79c58673e9084b5abc2a6b40b20dcaaf7456f8
Neelraj Patil <patilneelraj@gmail.com>
2024-11-21 17:19:49 -0500

    Time: 66 ms (14.36%) | Memory: 36.4 MB (6.01%) - LeetSync

--- 1909-buildings-with-an-ocean-view/buildings-with-an-ocean-view.py
+++ 1909-buildings-with-an-ocean-view/buildings-with-an-ocean-view.py
@@ -1,10 +1,21 @@
 class Solution:
     def findBuildings(self, heights: List[int]) -> List[int]:
-        res = []
-        max_height = float('-inf')
-        for i in range(len(heights)-1,-1,-1):
-            if heights[i] > max_height:
-                res.append(i)
-            max_height = max(max_height, heights[i])
+        # Monotonic Stack
+        stack = [] # [(height,index)]
+        for i in range(len(heights)):
+            while stack and stack[-1][0] <= heights[i]:
+                stack.pop()
+            stack.append((heights[i],i))
+            
+        return [s[1] for s in stack]
 
-        return [res[i] for i in range(len(res)-1,-1,-1)]
+
+
+        # res = []
+        # max_height = float('-inf')
+        # for i in range(len(heights)-1,-1,-1):
+        #     if heights[i] > max_height:
+        #         res.append(i)
+        #     max_height = max(max_height, heights[i])
+
+        # return [res[i] for i in range(len(res)-1,-1,-1)]
