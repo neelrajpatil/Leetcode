30d7e93e00ef0a38331e0436de0c6b7384737db4
Neelraj Patil <patilneelraj@gmail.com>
2024-11-22 18:19:08 -0500

    Time: 3 ms (40.00%) | Memory: 16.7 MB (19.68%) - LeetSync

--- 71-simplify-path/simplify-path.py
+++ 71-simplify-path/simplify-path.py
@@ -1,35 +1,56 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
+
         stack = []
 
-        # iterate thru each char
-        i = 0
-        curr_dir_file = ''
-        while i < len(path):
+        curr = ""
 
-            # if it is . then check next 2 chars
-            if path[i] == '.':
-                
-                if i+1 >= len(path) or path[i+1] == '/': # if it is .  
-                    i += 1
-                    continue   
-                elif path[i+1] == '.' and (i+2 >= len(path) or path[i+2] == '/'): # if it is ..
-                    if stack:
-                        stack.pop() # pop from stack
-                    i += 2
-                    continue
-            # if it is /
-            if path[i] == '/':
-                i += 1
-                continue
+        for c in path+'/':
+            if c == '/':
+                if curr == '..':
+                    if stack: stack.pop()
+                elif curr != "." and curr!= "":
+                    stack.append(curr)
+                curr = ""
             else:
-                while i < len(path) and path[i] != '/':
-                    curr_dir_file += path[i]
-                    i+=1
-                stack.append(curr_dir_file)
-                curr_dir_file = ''
+                curr += c
+
+        return '/' + '/'.join(stack)
+
+
+
+
+
+        # stack = []
+
+        # # iterate thru each char
+        # i = 0
+        # curr_dir_file = ''
+        # while i < len(path):
+
+        #     # if it is . then check next 2 chars
+        #     if path[i] == '.':
+                
+        #         if i+1 >= len(path) or path[i+1] == '/': # if it is .  
+        #             i += 1
+        #             continue   
+        #         elif path[i+1] == '.' and (i+2 >= len(path) or path[i+2] == '/'): # if it is ..
+        #             if stack:
+        #                 stack.pop() # pop from stack
+        #             i += 2
+        #             continue
+        #     # if it is /
+        #     if path[i] == '/':
+        #         i += 1
+        #         continue
+        #     else:
+        #         while i < len(path) and path[i] != '/':
+        #             curr_dir_file += path[i]
+        #             i+=1
+        #         stack.append(curr_dir_file)
+        #         curr_dir_file = ''
         
         
-        return '/'.join(['']+stack) if stack else '/'
+        # return '/'.join(['']+stack) if stack else '/'
 
                 
