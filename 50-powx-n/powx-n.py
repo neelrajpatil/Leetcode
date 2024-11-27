5ab9bdd4ccf8f4826e582e97d5e8cbfcb093a304
Neelraj Patil <patilneelraj@gmail.com>
2024-11-26 21:17:47 -0500

    Time: 1 ms (11.77%) | Memory: 17.5 MB (5.14%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -18,12 +18,15 @@ class Solution:
         """
 
         # handle edge cases
-        if x == 0 and n >= 1:
-            return 0
         if n == 0:
             return 1
+        if n < 0:
+            n = -n
+            x = 1/x
 
         
-
-        return x ** n
+        if n%2 == 0:
+            return self.myPow(x*x,n//2)
+        else:
+            return x * self.myPow(x*x,(n-1)//2)
         
